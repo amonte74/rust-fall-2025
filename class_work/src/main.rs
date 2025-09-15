b46199ba21955149a2968aed154ae1cc44230daa
Arturo Montemayor <montea5195@gmail.com>
2025-09-15 15:42:53 +0000

    9/15/2025 class work continued

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,21 +1,19 @@
+
+
+fn pattern_match_simple(num:u32) -> String {
+ 
+    match (num%3==0,num%5==0) {
+        (true,true) => "FizzBuzz".to_string(),
+        (true,false) => "Fizz".to_string(),
+        (false,true) => "Buzz".to_string(),
+        (false,false) => num.to_string(),
+    }
+
+}
+
 fn main() {
-    let x = 10;
-
+    for num in 1..20 {
+        println!("{}",pattern_match_simple(num));
+    }
     
-        let x = x + 10;
-        println!("{}",x);
-    
-
-    
-        let x = x + 10;
-        println!("{}",x);
-    
-
-    
-        let x = x + 10;
-        println!("{}",x);
-    
-    
-    println!("{}",x);
-
 }
