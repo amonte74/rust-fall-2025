9ee8a951349550a9b1af5861637ed27cb0525860
Arturo Montemayor <montea5195@gmail.com>
2025-09-17 15:31:31 +0000

    Commit for gitignore

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,19 +1,13 @@
 
 
-fn pattern_match_simple(num:u32) -> String {
- 
-    match (num%3==0,num%5==0) {
-        (true,true) => "FizzBuzz".to_string(),
-        (true,false) => "Fizz".to_string(),
-        (false,true) => "Buzz".to_string(),
-        (false,false) => num.to_string(),
-    }
+fn assignemnt1(){
 
+    // Temp converter
+    
 }
 
+
 fn main() {
-    for num in 1..20 {
-        println!("{}",pattern_match_simple(num));
-    }
-    
+    assignemnt1();
+
 }
