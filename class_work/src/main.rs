3f771c9162951c090041646057f64c939fa4dcb0
Arturo Montemayor <montea5195@gmail.com>
2025-09-24 15:35:05 +0000

    Rust ownership 9/24/25

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,13 +1,33 @@
 
-
-fn assignemnt1(){
-
-    // Temp converter
+fn append_region(mut word:String) {
+    word.push_str("RGV");
     
 }
 
-
 fn main() {
-    assignemnt1();
+    
+    let mut x = "UT".to_string();
+    append_region(&mut x);
+    println!("{}",x);
 
 }
+
+// fn main() {
+//     let mut word = "UT".to_string();
+
+//     {
+//     let mutate_word = &mut word;
+//     mutate_word.push_str("RGV")
+//     }
+
+//     println!("{}", word);
+// }
+
+// fn main() {
+//     let mut word = "UT".to_string(); 
+//     fn update(word: &mut String) {
+//         word.push_str("RGV");
+//     }
+//     update(&mut word);
+//     println!("{word}")
+// }
