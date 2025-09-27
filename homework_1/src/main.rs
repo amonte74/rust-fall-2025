96dc4dfd53ae47782769d7bc2e0bcd6f9094aa34
Arturo Montemayor <montea5195@gmail.com>
2025-09-27 02:14:27 +0000

    Rust Assignment Part 1

--- homework_1/src/main.rs
+++ homework_1/src/main.rs
@@ -1,3 +1,22 @@
+
+const FREEZING_POINT_F: f64 = 32.0;
+
+fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - FREEZING_POINT_F) * 5.0 / 9.0
+}
+
+fn celsius_to_fahrenheit(c: f64) -> f64 {
+    (c * 9.0 / 5.0) + FREEZING_POINT_F
+}
+
+
 fn main() {
-    println!("Hello, Brave World!");
+    let mut temp_f: f64 = 32.0; // starting temperature in Fahrenheit
+    println!("{:.1}°F = {:.1}°C", temp_f, fahrenheit_to_celsius(temp_f));
+
+    // Convert and print next 5 integer temperatures
+    for _ in 0..5 {
+        temp_f += 1.0;
+        println!("{:.1}°F = {:.1}°C", temp_f, fahrenheit_to_celsius(temp_f));
+    }
 }
