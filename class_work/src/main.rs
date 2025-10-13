98e5e798e70688119d9af1fa7aa402bd6ee455d9
Arturo Montemayor <montea5195@gmail.com>
2025-10-13 15:42:11 +0000

    10/13/25

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,29 +1,67 @@
-use std::io::{self, Read, Write};
+use std::process::Command;
+use std::io::{self, Write};
+use std::fs::File;
 
-struct Student {
-    name: String,
-    major: String,
+fn create_and_write_to_file(filename: &String) {
+    let mut file = File::create(&filename).unwrap();
+    writeln!(file, "Hello, Rust file operations!").unwrap();
+    writeln!(file, "This is a new line.").unwrap();
 }
 
-fn reading_from_console() {
+fn reading_from_console(msg:&String) -> String{
     let mut buffer = String::new();
 
-    print!("What's your name? ");
+    print!("{}",msg);
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut buffer).unwrap();
     let name = buffer.trim().to_string();
     buffer.clear();
+    return name;
+}
 
-    print!("What's your Major? ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut buffer).unwrap();
-    let major = buffer.trim().to_string();
-    buffer.clear();
+fn read_file_linux(filename:String) {
+    let output = Command::new("cat")
+        .arg(&filename)
+        .output()
+        .expect("Failed to execute command");
 
-    let student = Student { name, major };
-    println!("Hi {}, your major is {}!", student.name, student.major);
+    println!("Command output: {}", String::from_utf8_lossy(&output.stdout));
 }
 
 fn main() {
-    reading_from_console();
+
+    while true {
+        
+        let message = "What do you want to do today? Type 1 to create file, 2 to read from file, 3 to finish the program.".to_string();
+        println!("{}",message);
+        let choice = reading_from_console(&"Type the number: ".to_string()).parse().unwrap();
+
+        match choice {
+            1 => {
+                let message = "What is the name of your new file? ".to_string();
+                let filename = reading_from_console(&message);
+                create_and_write_to_file(&filename);
+
+            },
+            2 => {
+                let message = "What file to open?".to_string();
+                let filename = reading_from_console(&"What file to open? ".to_string()); //accept from the user
+                read_file_linux(filename); 
+            }
+            3 => break,
+            _ => {}
+        }
+    }
+    // ask the user if he wants to create a file and write
+    // => create a file and write inside of if
+    // accept everything from the console
+    //filename he wants to create and actual content
+    //refer to 03rust-file-operations.md
+    
+    // or if he wants to read from existent file
+
+    // accept from the user file he wants to open
+    // let message = "What file to open?"
+    // let filename = reading_from_console(&"What file to open? ".to_string()); //accept from the user
+    // read_file_linux(filename);
 }
