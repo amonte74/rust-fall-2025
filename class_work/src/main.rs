0a4832c1a6ade32b1578b4650a051e08eb577bfa
Arturo Montemayor <montea5195@gmail.com>
2025-10-15 15:45:31 +0000

    10/15/25 Enums

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,67 +1,47 @@
-use std::process::Command;
-use std::io::{self, Write};
-use std::fs::File;
-
-fn create_and_write_to_file(filename: &String) {
-    let mut file = File::create(&filename).unwrap();
-    writeln!(file, "Hello, Rust file operations!").unwrap();
-    writeln!(file, "This is a new line.").unwrap();
+#[derive(Debug)]
+enum Fruit {
+    Apple(String),
+    Banana(String),
+    Tomato(String),
 }
 
-fn reading_from_console(msg:&String) -> String{
-    let mut buffer = String::new();
-
-    print!("{}",msg);
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut buffer).unwrap();
-    let name = buffer.trim().to_string();
-    buffer.clear();
-    return name;
+struct Inventory {
+    fruit:Vec<Fruit>,
 }
 
-fn read_file_linux(filename:String) {
-    let output = Command::new("cat")
-        .arg(&filename)
-        .output()
-        .expect("Failed to execute command");
-
-    println!("Command output: {}", String::from_utf8_lossy(&output.stdout));
-}
-
-fn main() {
-
-    while true {
-        
-        let message = "What do you want to do today? Type 1 to create file, 2 to read from file, 3 to finish the program.".to_string();
-        println!("{}",message);
-        let choice = reading_from_console(&"Type the number: ".to_string()).parse().unwrap();
-
-        match choice {
-            1 => {
-                let message = "What is the name of your new file? ".to_string();
-                let filename = reading_from_console(&message);
-                create_and_write_to_file(&filename);
-
-            },
-            2 => {
-                let message = "What file to open?".to_string();
-                let filename = reading_from_console(&"What file to open? ".to_string()); //accept from the user
-                read_file_linux(filename); 
-            }
-            3 => break,
-            _ => {}
+impl Inventory {
+    fn available_fruits(&self){
+        for f in &self.fruit{
+            println!("{:?} :",f);
+            Self::tell_me_joke(f);
         }
     }
-    // ask the user if he wants to create a file and write
-    // => create a file and write inside of if
-    // accept everything from the console
-    //filename he wants to create and actual content
-    //refer to 03rust-file-operations.md
-    
-    // or if he wants to read from existent file
 
-    // accept from the user file he wants to open
-    // let message = "What file to open?"
-    // let filename = reading_from_console(&"What file to open? ".to_string()); //accept from the user
-    // read_file_linux(filename);
+    fn tell_me_joke(fruit:&Fruit){
+        match fruit {
+            Fruit::Apple(msg) => println!("{}",msg),
+            Fruit::Banana(msg) => println!("{}",msg),
+            Fruit::Tomato(msg) => println!("{}",msg),
+
+        }
+    }
 }
+
+fn main(){
+    let a = "An apple a day keeps the doctor away.".to_string();
+    let b = "A banana boosts energy in a peel.".to_string();
+    let t = "An tomato a day keeps the sunburn away.".to_string();
+    let fruits = vec![Fruit::Banana(b),Fruit::Apple(a),Fruit::Tomato(t)];
+    
+    let grocery_store = Inventory {
+        fruit:fruits,
+    };
+
+    grocery_store.available_fruits();
+}
+
+
+
+
+
+
