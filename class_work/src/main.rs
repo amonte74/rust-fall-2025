5c4fc8a03106695f1b05c1a605e35b36ebc7960b
Arturo Montemayor <montea5195@gmail.com>
2025-11-12 16:37:08 +0000

    11/12/25 Rust traits guide

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,23 +1,55 @@
-fn generics_method_definitions() {
-    struct File<T> {
-        name: String,
-        data: T,
-    }
-
-    impl<T> File<T> {
-        fn new(name: &str, content: T) -> File<T> {
-            File { name: String::from(name), data: content }
+fn last_lecture_problem_fixing(){
+    fn largest<T: PartialOrd + Copy>(list: &[T]) -> T { 
+        let mut largest = list[0]; // we need Copy trait to achieve that operation
+        
+        for &item in list.iter() {
+            if item > largest { // we need PartialOrd trait to be able to compare elements
+                largest = item;
+            }
         }
+        largest
     }
 
-    let textfile = File::new("lets'go", vec!["K'Maro".to_string()]);
-    let imagefile = File::new("MonaLisa", vec![0, 123, 255]);
+    // where clause
+    fn largest_2<T>(list: &[T]) -> T 
+        where T: PartialOrd + Copy,
+        {
+        let mut largest = list[0]; // we need Copy trait to achieve that operation
+        
+        for &item in list.iter() {
+            if item > largest { // we need PartialOrd trait to be able to compare elements
+                largest = item;
+            }
+        }
+        largest
+    }
 
-    println!("Textfile name {:?}. Textfile content {:?}", textfile.name, textfile.data);
-    println!("Imagefile name {:?}. Imagefile content {:?}", imagefile.name, imagefile.data);
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_2(&char_list);
+    println!("The largest char is {}", result);
 }
 
 fn main(){
-    generics_method_definitions();
+    last_lecture_problem_fixing();
 }
 
+// define 2 struct undegrad and grad student
+
+// define trait show info
+
+// grad student should have a thesis compnent
+// gpa and major will be shared
+
+// create another struct called Enrollment
+// inside enrollment store undegrad and grads together
+// implement show_info  for all enrolled student
+
+// everywhere use generics and traits, no if or match statement
+// program to behavior only
