02b81ad2e877bbbe3dcf8f1757d36fbc20ecb595
Arturo Montemayor <montea5195@gmail.com>
2025-11-13 02:16:05 +0000

    In class assignment

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,45 +1,3 @@
-fn last_lecture_problem_fixing(){
-    fn largest<T: PartialOrd + Copy>(list: &[T]) -> T { 
-        let mut largest = list[0]; // we need Copy trait to achieve that operation
-        
-        for &item in list.iter() {
-            if item > largest { // we need PartialOrd trait to be able to compare elements
-                largest = item;
-            }
-        }
-        largest
-    }
-
-    // where clause
-    fn largest_2<T>(list: &[T]) -> T 
-        where T: PartialOrd + Copy,
-        {
-        let mut largest = list[0]; // we need Copy trait to achieve that operation
-        
-        for &item in list.iter() {
-            if item > largest { // we need PartialOrd trait to be able to compare elements
-                largest = item;
-            }
-        }
-        largest
-    }
-
-
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest_2(&char_list);
-    println!("The largest char is {}", result);
-}
-
-fn main(){
-    last_lecture_problem_fixing();
-}
-
 // define 2 struct undegrad and grad student
 
 // define trait show info
@@ -53,3 +11,86 @@ fn main(){
 
 // everywhere use generics and traits, no if or match statement
 // program to behavior only
+
+trait ShowInfo {
+    fn show_info(&self);
+}
+
+struct Undergrad {
+    name: String,
+    gpa: f32,
+    major: String,
+}
+
+struct Grad {
+    name: String,
+    gpa: f32,
+    major: String,
+    thesis: String,
+}
+
+impl ShowInfo for Undergrad {
+    fn show_info(&self) {
+        println!(
+            "Undergrad Student -> Name: {}, GPA: {:.2}, Major: {}",
+            self.name, self.gpa, self.major
+        );
+    }
+}
+
+impl ShowInfo for Grad {
+    fn show_info(&self) {
+        println!(
+            "Grad Student -> Name: {}, GPA: {:.2}, Major: {}, Thesis: {}",
+            self.name, self.gpa, self.major, self.thesis
+        );
+    }
+}
+
+struct Enrollment<T: ShowInfo> {
+    students: Vec<T>,
+}
+
+impl<T: ShowInfo> Enrollment<T> {
+    fn new() -> Self {
+        Enrollment { students: Vec::new() }
+    }
+
+    fn enroll(&mut self, student: T) {
+        self.students.push(student);
+    }
+
+    fn show_all(&self) {
+        for s in &self.students {
+            s.show_info();
+        }
+    }
+}
+
+fn main(){
+    let u1 = Undergrad {
+        name: String::from("Alice"),
+        gpa: 3.8,
+        major: String::from("Computer Science"),
+    };
+
+    let g1 = Grad {
+        name: String::from("Bob"),
+        gpa: 3.9,
+        major: String::from("Electrical Engineering"),
+        thesis: String::from("AI-driven Circuits"),
+    };
+
+    let mut undergrad_enrollment = Enrollment::<Undergrad>::new();
+    undergrad_enrollment.enroll(u1);
+
+    let mut grad_enrollment = Enrollment::<Grad>::new();
+    grad_enrollment.enroll(g1);
+
+    println!("--- Undergrad Students ---");
+    undergrad_enrollment.show_all();
+
+    println!("\n--- Grad Students ---");
+    grad_enrollment.show_all();
+}
+
