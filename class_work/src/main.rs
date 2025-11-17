fd0d5116cac2f7af0a1fb86954e77350dd831c9b
Arturo Montemayor <montea5195@gmail.com>
2025-11-17 16:44:03 +0000

    11/17/25 Rust closures and box pointer

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,96 +1,44 @@
-// define 2 struct undegrad and grad student
 
-// define trait show info
-
-// grad student should have a thesis compnent
-// gpa and major will be shared
-
-// create another struct called Enrollment
-// inside enrollment store undegrad and grads together
-// implement show_info  for all enrolled student
-
-// everywhere use generics and traits, no if or match statement
-// program to behavior only
-
-trait ShowInfo {
-    fn show_info(&self);
-}
-
-struct Undergrad {
-    name: String,
-    gpa: f32,
-    major: String,
-}
-
-struct Grad {
-    name: String,
-    gpa: f32,
-    major: String,
-    thesis: String,
-}
-
-impl ShowInfo for Undergrad {
-    fn show_info(&self) {
-        println!(
-            "Undergrad Student -> Name: {}, GPA: {:.2}, Major: {}",
-            self.name, self.gpa, self.major
-        );
+fn box_polymorphism() {
+    use core::fmt::Debug;
+    
+    trait Animal: Debug {
+        fn sound(&self) -> String;
     }
-}
-
-impl ShowInfo for Grad {
-    fn show_info(&self) {
-        println!(
-            "Grad Student -> Name: {}, GPA: {:.2}, Major: {}, Thesis: {}",
-            self.name, self.gpa, self.major, self.thesis
-        );
-    }
-}
-
-struct Enrollment<T: ShowInfo> {
-    students: Vec<T>,
-}
-
-impl<T: ShowInfo> Enrollment<T> {
-    fn new() -> Self {
-        Enrollment { students: Vec::new() }
-    }
-
-    fn enroll(&mut self, student: T) {
-        self.students.push(student);
-    }
-
-    fn show_all(&self) {
-        for s in &self.students {
-            s.show_info();
+    // inside of the struct create a field called name
+    // and beside sounds your animal should print "hey my name is {}",name
+    
+    #[derive(Debug)]
+    struct Dog{
+        name:String,
+    };
+    
+    impl Animal for Dog {
+        fn sound(&self) -> String {
+            "Woof woof. My name is {}".to_string()
         }
     }
+    
+    #[derive(Debug)]
+    struct Cat;
+    
+    impl Animal for Cat {
+        fn sound(&self) -> String {
+            "Meow meow".to_string()
+        }
+    }
+
+    
+    let mut zoo: Vec<Box<dyn Animal>> = Vec::new();
+    
+    zoo.push(Box::new(Dog{name:"Lucy"}));
+    zoo.push(Box::new(Cat{}));
+    
+    for animal in zoo {
+        println!("{}", animal, animal.sound());
+    }
 }
 
-fn main(){
-    let u1 = Undergrad {
-        name: String::from("Alice"),
-        gpa: 3.8,
-        major: String::from("Computer Science"),
-    };
-
-    let g1 = Grad {
-        name: String::from("Bob"),
-        gpa: 3.9,
-        major: String::from("Electrical Engineering"),
-        thesis: String::from("AI-driven Circuits"),
-    };
-
-    let mut undergrad_enrollment = Enrollment::<Undergrad>::new();
-    undergrad_enrollment.enroll(u1);
-
-    let mut grad_enrollment = Enrollment::<Grad>::new();
-    grad_enrollment.enroll(g1);
-
-    println!("--- Undergrad Students ---");
-    undergrad_enrollment.show_all();
-
-    println!("\n--- Grad Students ---");
-    grad_enrollment.show_all();
+fn main() {
+    box_polymorphism();
 }
-
