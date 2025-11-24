4cd0ffe0843b5e035ddb743999d7488686d297f2
Arturo Montemayor <montea5195@gmail.com>
2025-11-24 16:39:19 +0000

    11/24/2025

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,24 +1,43 @@
-fn process_vector<F>(vec: Vec<i32>, f: F) -> Vec<i32>
-where
-    F: Fn(i32) -> i32,
-{
-    // Your implementation here
-    vec.into_iter().map(f).collect()
+use std::rc::Rc;
+use std::cell::RefCell;
+
+fn sharing_resource_refcell_count() {
+    struct FamilyMember {
+        tv: Rc<RefCell<TV>>,
+    }
+
+    #[derive(Debug)]
+    struct TV {
+        channel: String,
+    }
+
+    fn member_start_watch_tv() -> FamilyMember {
+        let tv_is_on = Rc::new(RefCell::new(TV{channel:"BBC".to_string()}));
+        FamilyMember {
+            tv: tv_is_on, 
+        }
+    }
+
+    let dad = member_start_watch_tv();
+    let mom = FamilyMember { tv: Rc::clone(&dad.tv) };
+    println!("TV channel for mom {:?}", mom.tv);
+
+    let mut remote_control = dad.tv.borrow_mut();
+    println!("TV channel {:?}", remote_control);
+
+    remote_control.channel = "MTV".to_string();
+    println!("TV channel {:?}", remote_control);
+    drop(remote_control);
+    println!("TV channel for mom {:?}", mom.tv);
+
+    //add yourself as watchin person
+    let myself = FamilyMember { tv: Rc::clone(&dad.tv) };
+    //and show what channe; you are watching
+    println!("TV channel for me {:?}", myself.tv);
+    //show how many people are watching the TV
+    println!("People are watching {}",Rc::strong_count(&myself.tv));
 }
 
 fn main() {
-    let numbers = vec![1, 2, 3];
-
-    let doubled = process_vector(numbers.clone(), |x| {
-        // Implement: multiply each number by 2
-        x
-    });
-
-    let replaced = process_vector(numbers, |x| {
-        // Implement: if number > 2, replace with 0, else keep number
-        x
-    });
-
-    println!("Doubled: {:?}", doubled);
-    println!("Replaced: {:?}", replaced);
+    sharing_resource_refcell_count();
 }
