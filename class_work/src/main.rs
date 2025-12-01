9ab9096124c41a892380992f9fcbb37ea05c01cf
Arturo Montemayor <montea5195@gmail.com>
2025-12-01 16:39:31 +0000

    12/1/2025 (Week 9 aIntro to concurrency)

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,43 +1,17 @@
-use std::rc::Rc;
-use std::cell::RefCell;
+use std::thread;
+use std::time::Duration;
 
-fn sharing_resource_refcell_count() {
-    struct FamilyMember {
-        tv: Rc<RefCell<TV>>,
-    }
-
-    #[derive(Debug)]
-    struct TV {
-        channel: String,
-    }
-
-    fn member_start_watch_tv() -> FamilyMember {
-        let tv_is_on = Rc::new(RefCell::new(TV{channel:"BBC".to_string()}));
-        FamilyMember {
-            tv: tv_is_on, 
-        }
-    }
-
-    let dad = member_start_watch_tv();
-    let mom = FamilyMember { tv: Rc::clone(&dad.tv) };
-    println!("TV channel for mom {:?}", mom.tv);
-
-    let mut remote_control = dad.tv.borrow_mut();
-    println!("TV channel {:?}", remote_control);
-
-    remote_control.channel = "MTV".to_string();
-    println!("TV channel {:?}", remote_control);
-    drop(remote_control);
-    println!("TV channel for mom {:?}", mom.tv);
-
-    //add yourself as watchin person
-    let myself = FamilyMember { tv: Rc::clone(&dad.tv) };
-    //and show what channe; you are watching
-    println!("TV channel for me {:?}", myself.tv);
-    //show how many people are watching the TV
-    println!("People are watching {}",Rc::strong_count(&myself.tv));
-}
 
 fn main() {
-    sharing_resource_refcell_count();
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
 }
