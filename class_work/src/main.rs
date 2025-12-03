71d3eff432d28303c1747bf1fb7bde29d087a196
Arturo Montemayor <montea5195@gmail.com>
2025-12-03 16:45:32 +0000

    12/3/2025 concurrency basic upper basic

--- class_work/src/main.rs
+++ class_work/src/main.rs
@@ -1,17 +1,33 @@
+
 use std::thread;
 use std::time::Duration;
+fn share_data_between_threads_through_channel() {
+    // multiple producer, single consumer
+    use std::sync::mpsc;
 
+    println!("Concurrency");
+    let (sender,receiver) = mpsc::channel(); // notice tuple unpacking
+
+    let thread = {
+        std::thread::spawn(move ||{
+            let steps = receiver.recv().unwrap();
+            for step in 1..=steps {
+                std::thread::sleep(std::time::Duration::from_secs(1));
+                println!("Thread step {}",step);
+            }
+            "Goodbye!" // important thread could return values
+        })
+    };
+
+    println!("Spawned a thread!");
+    sender.send(5).unwrap();// if message did not reach reciever, you get err
+    std::thread::sleep(std::time::Duration::from_secs(3));
+    println!("Main thread slept for 3 seconds");
+    let result = thread.join().unwrap();
+    println!("Thread returned: {:?}", result);
+    
+}
 
 fn main() {
-    thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
-
-    for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
-    }
+    share_data_between_threads_through_channel();
 }
